a22a321d481fac4485864d4b93572c239c7d49ad
mickymck <mickymckeon@gmail.com>
2019-10-22 16:55:01 -0400

    got all my goals done, and now I get to play around

--- notes/models.py
+++ notes/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Note(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
